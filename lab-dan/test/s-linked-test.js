f65e7ee59a31d0134295b0b6af885e55c22fdc73
Dan Peters <daniel.r.peters@icloud.com>
2017-01-04 21:28:44 -0800

    implemented single-link tests

--- lab-dan/test/s-linked-test.js
+++ lab-dan/test/s-linked-test.js
@@ -1,30 +1,62 @@
 'use strict'
 
 const expect = require('chai').expect
-const SinglyLinkedList = require('..lib/single/s-linked')
-let sll = new SinglyLinkedList()
+const SinglyLinkedList = require('../lib/single/s-linked')
+let sll = undefined
+let [a, b] = [1, 2]
+
 
 describe('singly-linked list module', function() {
   describe('prepend', function() {
+    sll = new SinglyLinkedList()
     it('should create a new node and add to sll.head when list is empty', function() {
-      let n = 1
-      sll.prepend(n)
-      expect(sll.head.value).to.be.equal(n)
+      sll.prepend(a)
+      expect(sll.head.value).to.equal(a)
       expect(sll.head.next).to.be.null
     })
     it('should create a new node and add to sll.head when list has existing node(s)', function() {
-      let n = 2
-      sll.prepend(n)
-      expect(sll.head.value).to.be.equal(n)
-      expect(sll.head.next).to.not.be.null
+      sll.prepend(b)
+      expect(sll.head.value).to.equal(b)
+      expect(sll.head.next.value).to.equal(a)
     })
   })
 
   describe('append', function() {
+    sll = new SinglyLinkedList()
+    it('should create a new node and add to sll.head when list is empty', function() {
+      sll.append(a)
+      expect(sll.head.value).to.equal(a)
+      expect(sll.head.next).to.be.null
+    })
 
+    it('should create a new node and add to end of list with existing node', function() {
+      sll.append(b)
+      expect(sll.head.value).to.equal(a)
+      expect(sll.head.next.value).to.equal(b)
+    })
   })
 
-  describe('remove', function() {
+  describe('removeHead', function() {
+    sll = new SinglyLinkedList([a, b])
+    it('should remove the head node and replace it with next', function() {
+      expect(sll.removeHead()).to.equal(a)
+      expect(sll.head.value).to.equal(b)
+    })
+    it('should remove the head node and set list as null when last node', function() {
+      expect(sll.removeHead()).to.equal(b)
+      expect(sll.removeHead()).to.be.undefined
+    })
+  })
 
+  describe('removeTail', function() {
+    sll = new SinglyLinkedList([a, b])
+    it('should remove the tail node', function() {
+      expect(sll.removeTail()).to.equal(b)
+      expect(sll.head.value).to.equal(a)
+    })
+    it('should remove the head node and set list as null when last node', function() {
+      expect(sll.removeTail()).to.equal(a)
+      expect(sll.removeTail()).to.be.undefined
+    })
   })
 })
