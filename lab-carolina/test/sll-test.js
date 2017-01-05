2dc2554bb1fd8cec9cbccd760269417a194fed0e
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-01-04 23:50:22 -0800

    fixed some things

--- lab-carolina/test/sll-test.js
+++ lab-carolina/test/sll-test.js
@@ -5,9 +5,10 @@ const SLL = require('../lib/sll.js');
 
 describe('Singly Linked List', function() {
   describe('prepend()', function() {
+    let sll;
     before( () => { //by doing this, you now have access to SLL inside of the blocks
-    let sll = new SLL() //this lets sll be able to be grabbed by line 12.
-    })
+      sll = new SLL(); //this lets sll be able to be grabbed by line 12.
+    });
     it('should create a new node as SLL.head when list is empty', function(){
       expect(sll.head).to.equal(null);
       sll.prepend(25);
